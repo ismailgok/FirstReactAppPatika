8b7657cd8315b328732ecdbdefc350892db70a4c
İsmail <ismayilgok16@gmail.com>
2022-01-21 23:49:51 +0300

    State lerde Array Kullanımı

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import { useState } from 'react';
 function App(){
 
     const [name,setName] = useState("Ahmet");
-    const [age, setAge] = useState(23) 
+    const [age, setAge] = useState(23);
+    const [friends, setFriends] = useState(["Ahmet","Mehmet","Yusuf"]);
+
 
     return(
 
@@ -17,6 +19,21 @@ function App(){
         <button onClick={() => setName("İsmail")}>İsmi Güncelle</button>
         
         <button onClick={() => setAge(25)}>Yaşı Güncelle</button>
+
+        <br/>
+        <br/>
+        <h1>Friends</h1>
+
+        {
+            friends.map((friend,index) => <div key={index}> {friend} </div>)
+
+        }
+        
+        <br />
+
+        <button onClick={() => setFriends([...friends,"Ayşe"])}>new friend</button>
+
+
        </div>
 
 
